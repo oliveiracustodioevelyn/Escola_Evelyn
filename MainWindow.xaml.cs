d9bfd20d065699e2baf1cb5232d1307879b383ef
Evelyn <evelynmarinalimaeve@gmail.com>
2022-04-26 10:09:57 -0400

    att

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace AtivEscola
 {
@@ -24,7 +25,70 @@ namespace AtivEscola
         {
             InitializeComponent();
 
-            string nome = BxNome.Text.ToString();
+
         }
+            
+
+        private void BtSalvar_Click(object sender, RoutedEventArgs e)
+        {
+            string data_criacao = TxtData.SelectedDate?.ToString("yyyy-mm-dd");
+            string fantasia = TxtNomeFantasia.Text;
+            string razao = TxtRazao.Text;
+            string cnpj = TxtCNPJ.Text;
+            string inscricao = TxtInsc.Text;
+            string data = TxtData.Text;
+            string resp = TxtNome.Text;
+            string resp_tel = TxtTelefoneResp.Text;
+            string email = TxtEmail.Text;
+            string numero = TxtN.Text;
+            string bairro = TxtBairro.Text;
+            string comp = TxtCompremento.Text;
+            string cep = TxtCep.Text;
+            string cidade = TxtCidade.Text;
+            string estado = TxtEstado.Text;
+
+
+                var conexao = new MySqlConnection("server=localhost; database=BD_ESCOLA; port=3360; user=root; password=root");
+
+                conexao.Open();
+
+                try
+                {
+                    conexao.Close();
+                    var comando = conexao.CreateCommand();
+                    comando.CommandText = "INSERT INTO Escola values(null,@nome, @razao, @cnpj,@inscricao, @tipo, @data_criacao, @resp, " +
+                    "@resp_tel, @email, @numero, @bairro, @complemento, @cep, @cidade, @estado;)";
+
+                comando.Parameters.AddWithValue("@nome", fantasia);
+                comando.Parameters.AddWithValue("@razao", razao);
+                comando.Parameters.AddWithValue("@cnpj", cnpj);
+                comando.Parameters.AddWithValue("@inscricao", inscricao);
+                        comando.Parameters.AddWithValue("@tipo", RadioTipoPri);
+                        comando.Parameters.AddWithValue("@tipo", RadioTipoPub);
+                comando.Parameters.AddWithValue("@data_criacao", data);
+                comando.Parameters.AddWithValue("@resp", resp);
+                comando.Parameters.AddWithValue("@resp_tel", resp_tel);
+                comando.Parameters.AddWithValue("@email", email);
+                comando.Parameters.AddWithValue("@bairro", bairro);
+                comando.Parameters.AddWithValue("@complemento", comp);
+                comando.Parameters.AddWithValue("@inscricao", inscricao);
+                comando.Parameters.AddWithValue("@cep", cep);
+                comando.Parameters.AddWithValue("@cidade", cidade);
+                comando.Parameters.AddWithValue("@estado", estado);
+
+
+                    var resultado = comando.ExecuteNonQuery();
+                    if (resultado > 0)
+                    {
+                        MessageBox.Show("Registro salvo com socesso!");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+        }
+        
     }
 }
